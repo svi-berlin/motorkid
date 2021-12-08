7f87e59cf3584aaa9a283fe60dfb8375cd77d59e
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-08 19:08:25 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
     if (receivedNumber == 1) {
+        music.playTone(131, music.beat(BeatFraction.Sixteenth))
         nachLinks()
     } else {
         if (receivedNumber == 2) {
+            music.playTone(330, music.beat(BeatFraction.Sixteenth))
             nachRechts()
+        } else {
+            if (receivedNumber == 3) {
+                music.playTone(988, music.beat(BeatFraction.Sixteenth))
+                gerade()
+            }
         }
     }
 })
@@ -13,14 +21,10 @@ function gerade () {
 function nachLinks () {
     motors.dualMotorPower(Motor.A, 50)
     motors.dualMotorPower(Motor.B, 0)
-    basic.pause(500)
-    gerade()
 }
 function nachRechts () {
     motors.dualMotorPower(Motor.A, 0)
     motors.dualMotorPower(Motor.B, 50)
-    basic.pause(500)
-    gerade()
 }
 radio.setGroup(1)
 basic.showIcon(IconNames.Heart)
