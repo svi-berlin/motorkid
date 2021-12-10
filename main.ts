895815295afda9bee1f0eac07d862f92596ea8d0
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-10 23:48:17 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,28 +1,26 @@
 function gerade () {
     motors.dualMotorPower(Motor.AB, motorLeistung)
-    basic.setLedColor(0x00ff00)
 }
 function nachLinks () {
     motors.dualMotorPower(Motor.A, motorLeistung)
     motors.dualMotorPower(Motor.B, 0)
-    basic.setLedColor(0xff0000)
-    basic.pause(500)
 }
 function nachRechts () {
     motors.dualMotorPower(Motor.A, 0)
     motors.dualMotorPower(Motor.B, motorLeistung)
-    basic.setLedColor(0xffff00)
-    basic.pause(500)
 }
 let motorLeistung = 0
 motorLeistung = 60
 basic.forever(function () {
-    if (grove.measureInCentimeters(DigitalPin.C16) < 80) {
+    if (grove.measureInCentimeters(DigitalPin.C16) < 20) {
+        basic.setLedColor(0xff0000)
         nachRechts()
     } else {
-        if (grove.measureInCentimeters(DigitalPin.C16) < 120) {
+        if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
+            basic.setLedColor(0xffff00)
             nachLinks()
         } else {
+            basic.setLedColor(0x00ff00)
             gerade()
         }
     }
