3c7003ae627a11ad127f3cc6e8745cc09fa0651b
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-10 23:42:49 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,19 +6,21 @@ function nachLinks () {
     motors.dualMotorPower(Motor.A, motorLeistung)
     motors.dualMotorPower(Motor.B, 0)
     basic.setLedColor(0xff0000)
+    basic.pause(500)
 }
 function nachRechts () {
     motors.dualMotorPower(Motor.A, 0)
     motors.dualMotorPower(Motor.B, motorLeistung)
     basic.setLedColor(0xffff00)
+    basic.pause(500)
 }
 let motorLeistung = 0
-motorLeistung = 45
+motorLeistung = 60
 basic.forever(function () {
-    if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
+    if (grove.measureInCentimeters(DigitalPin.C16) < 80) {
         nachRechts()
     } else {
-        if (grove.measureInCentimeters(DigitalPin.C16) < 100) {
+        if (grove.measureInCentimeters(DigitalPin.C16) < 120) {
             nachLinks()
         } else {
             gerade()
