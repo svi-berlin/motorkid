310a108fee437893dda82b6f677e247805a8370a
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-18 16:27:03 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,78 +1,27 @@
-radio.onReceivedNumber(function (receivedNumber) {
-    remoteControl = 1
-    basic.showNumber(receivedNumber)
-    if (receivedNumber == 1) {
-        motors.dualMotorPower(Motor.A, 0)
-        motors.dualMotorPower(Motor.B, motorLeistung)
-        beideMotorenStandard()
-    } else {
-        if (receivedNumber == 2) {
-            motors.dualMotorPower(Motor.B, 0)
-            motors.dualMotorPower(Motor.A, motorLeistung)
-            beideMotorenStandard()
-        } else {
-            if (receivedNumber == 3) {
-                motors.dualMotorPower(Motor.A, 0)
-                motors.dualMotorPower(Motor.B, 0)
-            } else {
-                if (receivedNumber == 4) {
-                    motors.dualMotorPower(Motor.A, 100)
-                    motors.dualMotorPower(Motor.B, 100)
-                    beideMotorenStandard()
-                }
-            }
-        }
-    }
-})
 function gerade () {
     motors.dualMotorPower(Motor.AB, motorLeistung)
-    ausweichen = 0
 }
 function nachLinks () {
-    if (ausweichen < 5) {
-        motors.dualMotorPower(Motor.A, 100)
-        motors.dualMotorPower(Motor.B, 0)
-        ausweichen += 1
-    } else {
-        ausweichen = 0
-        nachRechts()
-    }
-}
-function beideMotorenStandard () {
-    basic.pause(1000)
-    motors.dualMotorPower(Motor.A, motorLeistung)
-    motors.dualMotorPower(Motor.B, motorLeistung)
+    motors.dualMotorPower(Motor.A, 100)
+    motors.dualMotorPower(Motor.B, 0)
 }
 function nachRechts () {
-    if (ausweichen < 5) {
-        motors.dualMotorPower(Motor.A, 0)
-        motors.dualMotorPower(Motor.B, 100)
-        ausweichen += 1
-    } else {
-        ausweichen = 0
-        nachLinks()
-    }
+    motors.dualMotorPower(Motor.A, 0)
+    motors.dualMotorPower(Motor.B, 100)
 }
-let remoteControl = 0
-let ausweichen = 0
 let motorLeistung = 0
-radio.setGroup(1)
-motorLeistung = 60
-ausweichen = 0
-remoteControl = 0
+motorLeistung = 100
 basic.forever(function () {
-    if (remoteControl == 0) {
-        if (grove.measureInCentimeters(DigitalPin.C16) < 10) {
-            basic.setLedColor(0xff0000)
-            nachRechts()
+    if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
+        basic.setLedColor(0xff0000)
+        nachRechts()
+    } else {
+        if (grove.measureInCentimeters(DigitalPin.C16) < 80) {
+            basic.setLedColor(0xffff00)
+            nachLinks()
         } else {
-            if (grove.measureInCentimeters(DigitalPin.C16) < 30) {
-                basic.setLedColor(0xffff00)
-                nachLinks()
-            } else {
-                basic.setLedColor(0x00ff00)
-                gerade()
-            }
+            basic.setLedColor(0x00ff00)
+            gerade()
         }
     }
 })
