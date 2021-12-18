671e0b707b03be2890e1fcee971423056a5eb0c0
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-18 14:02:54 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,40 @@
 function gerade () {
     motors.dualMotorPower(Motor.AB, motorLeistung)
+    ausweichen = 0
+    basic.showNumber(ausweichen)
 }
 function nachLinks () {
-    motors.dualMotorPower(Motor.A, 100)
-    motors.dualMotorPower(Motor.B, 0)
+    if (ausweichen < 5) {
+        motors.dualMotorPower(Motor.A, 100)
+        motors.dualMotorPower(Motor.B, 0)
+        ausweichen += 1
+        basic.showNumber(ausweichen)
+    } else {
+        ausweichen = 0
+        nachRechts()
+    }
 }
 function nachRechts () {
-    motors.dualMotorPower(Motor.A, 0)
-    motors.dualMotorPower(Motor.B, 100)
+    if (ausweichen < 5) {
+        motors.dualMotorPower(Motor.A, 0)
+        motors.dualMotorPower(Motor.B, 100)
+        ausweichen += 1
+        basic.showNumber(ausweichen)
+    } else {
+        ausweichen = 0
+        nachLinks()
+    }
 }
+let ausweichen = 0
 let motorLeistung = 0
-motorLeistung = 60
+motorLeistung = 100
+ausweichen = 0
 basic.forever(function () {
-    if (grove.measureInCentimeters(DigitalPin.C16) < 20) {
+    if (grove.measureInCentimeters(DigitalPin.C16) < 10) {
         basic.setLedColor(0xff0000)
         nachRechts()
     } else {
-        if (grove.measureInCentimeters(DigitalPin.C16) < 50) {
+        if (grove.measureInCentimeters(DigitalPin.C16) < 30) {
             basic.setLedColor(0xffff00)
             nachLinks()
         } else {
