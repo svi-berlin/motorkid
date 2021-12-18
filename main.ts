a1ae361aab75bc98f409bc9b7a64356dc2aa6938
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-18 14:27:46 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    remoteControl = 1
     if (receivedNumber == 1) {
         motors.dualMotorPower(Motor.A, 0)
-        basic.pause(500)
-        motors.dualMotorPower(Motor.A, 100)
     } else {
         if (receivedNumber == 2) {
             motors.dualMotorPower(Motor.B, 0)
-            basic.pause(500)
-            motors.dualMotorPower(Motor.B, 100)
         } else {
             if (receivedNumber == 3) {
                 motors.dualMotorPower(Motor.A, 0)
@@ -45,22 +42,27 @@ function nachRechts () {
         nachLinks()
     }
 }
+let remoteControl = 0
 let ausweichen = 0
 let motorLeistung = 0
 radio.setGroup(1)
 motorLeistung = 100
 ausweichen = 0
+remoteControl = 0
 basic.forever(function () {
-    if (grove.measureInCentimeters(DigitalPin.C16) < 10) {
-        basic.setLedColor(0xff0000)
-        nachRechts()
-    } else {
-        if (grove.measureInCentimeters(DigitalPin.C16) < 30) {
-            basic.setLedColor(0xffff00)
-            nachLinks()
+    if (remoteControl == 0) {
+        if (grove.measureInCentimeters(DigitalPin.C16) < 10) {
+            basic.setLedColor(0xff0000)
+            nachRechts()
         } else {
-            basic.setLedColor(0x00ff00)
-            gerade()
+            if (grove.measureInCentimeters(DigitalPin.C16) < 30) {
+                basic.setLedColor(0xffff00)
+                nachLinks()
+            } else {
+                basic.setLedColor(0x00ff00)
+                gerade()
+            }
         }
     }
+    basic.showNumber(remoteControl)
 })
